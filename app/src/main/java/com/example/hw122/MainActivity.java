f52259952757de3ef76926cb1c80c8da75178ad1
meowka <Pustygin.a@mail.ru>
2020-03-23 00:05:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hw122/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hw122;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Random random = new Random();
+        final int i = random.nextInt(101);
+        Button btnPrev = (Button) findViewById(R.id.btnPrev);
+        Button btnNext = (Button) findViewById(R.id.btnNext);
+        final TextView URL = (TextView) findViewById(R.id.URL);
+        URL.setText("http://myfile.org/" + i);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
